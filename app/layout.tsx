c94335ccba2345af1f9dc9ff3e8e78e86978fa05
Azizhecker <abdul.210170135@mhs.unimal.ac.id>
2026-01-28 12:10:15 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,17 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
+import type { Metadata } from "next";
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Checklist KPPN Kota Lhokseumawe',
   description: 'Digitalisasi Pemeliharaan Gedung & Kendaraan',
+  icons: {
+    icon: "/favicon.ico", // Ganti dengan nama file logo Anda di folder public
+    shortcut: "/favicon.ico",
+    apple: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
